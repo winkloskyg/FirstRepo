7a00715c3b8a707c97538e26114ba676cd2a313f
winkloskyg <gwinklosky@hybrid-magnolia.fgm.com>
2013-08-22 13:02:24 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordgame::Application.config.secret_token = 'a486132fc367f6c20f2bb14c4906009a5b08295bb9313cc88be4cd789ad16c05ae6081722973d752d39867046031817efeb93129c74b2e8c974217d222936aed'
